a10bdcef1df6e7c87a3ca74ad3169830fb4708aa
eva <92198779+EvangeliaIatridou@users.noreply.github.com>
2026-07-17 19:50:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { Start } from './scenes/Start.js';
+import { abouthegame } from './scenes/abouthegame.js';
 import { defaultnone } from './scenes/defaultnone.js';
 import { firstscene } from './scenes/firstscene.js';
 import { kidscene1 } from './scenes/kidscene1.js';
@@ -15,6 +16,20 @@ import { playground2 } from './scenes/playground2.js';
 import { classroom } from './scenes/classroom.js';
 import { young2 } from './scenes/young2.js';
 
+import { adult1 } from './scenes/adult1.js';
+import { adult2 } from './scenes/adult2.js';
+import { work1 } from './scenes/work1.js';
+import { work2balcony } from './scenes/work2balcony.js';
+import { adultout1 } from './scenes/adultout1.js';
+
+import { endscene } from './scenes/endscene.js';
+// import { schoolinit } from './scenes/schoolinit.js';
+// import { gang } from './scenes/gang.js';
+// import { yellowcyan } from './scenes/yellowcyan.js';
+// import { playground2 } from './scenes/playground2.js';
+// import { classroom } from './scenes/classroom.js';
+// import { young2 } from './scenes/young2.js';
+
 
 const config = {
     type: Phaser.AUTO,
@@ -26,8 +41,11 @@ const config = {
     backgroundColor: '#000000',
     pixelArt: false,
     scene: [
-        Start,defaultnone,firstscene,pickinitcolors,kidscene1,playground,kidscene2,colorgatherer,
-        young1,schoolinit,gang,yellowcyan,playground2,classroom,young2
+        Start,defaultnone,firstscene,pickinitcolors,abouthegame,
+        kidscene1,playground,kidscene2,colorgatherer,
+        young1,schoolinit,gang,yellowcyan,playground2,classroom,young2,
+        adult1,adult2,work1,work2balcony,adultout1,
+        endscene
     ],
     scale: {
         mode: Phaser.Scale.FIT,
